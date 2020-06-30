a927e33e268eb018be77cc7a7b6f210e083baf48
paulamarv8 <paulamarv8@users.noreply.github.com>
2020-06-30 12:05:39 +0200

    Actualizaciñon del Service
    HashMap de jugadores añadida y los métodos para obtener los jugadores

--- src/main/java/es/codeurjc/ais/tictactoe/StatisticsService.java
+++ src/main/java/es/codeurjc/ais/tictactoe/StatisticsService.java
@@ -1,8 +1,23 @@
 package es.codeurjc.ais.tictactoe;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class StatisticsService {
+	private Map<Integer,Player> jugadores = new HashMap<Integer,Player>();
 	
+	public StatisticsService(){};
+	
+	public void addPlayerSer(Player player) {
+		this.jugadores.put(player.getId(), player);
+	}
+	
+	public List<Player> getPlayers(){
+		return new ArrayList<Player>(jugadores.values());
+	}
 }
